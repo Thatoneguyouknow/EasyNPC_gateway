4e28cbbbc60db10132c436819b8dabef942ed912
Benngy <benjistaman.12@gmail.com>
2024-01-25 18:10:03 -0700

    Creating Name Scheming Each RaceNameScheme will have various NameSchemes so that we can create names based on race/subrace

---
+++ src/main/java/com/npc/NamingLogic/RaceNameScheme.java
@@ -0,0 +1,21 @@
+package com.npc.NamingLogic;
+
+import java.util.ArrayList;
+
+// This will be the default naming scheme for each race.
+// Races will need to store the number that corresponds to their assigned race name scheme
+// From there each Name Scheme will have an ID
+// If the race has subrace dependent names, they will be bound by that ID
+// Otherwise the ID will go unused
+
+public class RaceNameScheme {
+    private int nameID;
+    private ArrayList<NameScheme> firstNames;
+    private ArrayList<NameScheme> lastNames;
+
+    public RaceNameScheme(int name_id, ArrayList<NameScheme> firsts, ArrayList<NameScheme> lasts) {
+        this.nameID = name_id;
+        this.firstNames = firsts;
+        this.lastNames = lasts;
+    }
+}
